0eb86e7290170906f2777b1b444fbcc50cf0276c
assaft753 <assaft753@gmail.com>
2018-05-20 08:08:29 +0300

    Update Image View

--- MemoryGame/View Controllers/UpdateImagesViewController.swift
+++ MemoryGame/View Controllers/UpdateImagesViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Assaf Tayouri on 18/05/2018.
 //  Copyright © 2018 Assaf Tayouri. All rights reserved.
 //
-
 import UIKit
 
 class UpdateImagesViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
@@ -33,13 +32,9 @@ class UpdateImagesViewController: UIViewController,UICollectionViewDelegate,UICo
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let imageViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "image", for: indexPath) as! ImageUpdateCollectionViewCell
+        let imageViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "update image cell", for: indexPath) as! ImageUpdateCollectionViewCell
         imageViewCell.imageView.image = self.currentImages[indexPath.item]
         return imageViewCell
-    }
-    
-    @objc func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
-        return UIEdgeInsetsMake(0, 0, 0, 0)
     }
     
 }
